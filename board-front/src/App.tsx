cf48c9f958ca0170d4c176bac5e7f9f38c228b5c
kjwjwjw <tbs03112@naver.com>
2024-04-26 22:06:33 +0900

    댓글 컴포넌트

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import { latestBoardListMock, top3BoardListMock } from 'mocks';
+import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
 import Top3Item from 'components/Top3Item';
+import CommentItem from 'components/CommentItem';
+import FavoriteItem from 'components/FavoriteItem';
 
 function App() {
   return (
     <>
-      <div style={{ display: 'flex', justifyContent: 'center' , gap: '24px'}}>
-          {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem}/>)}
-    </div>
+    <div style={{ display: 'flex',  columnGap: '30px', rowGap: '20px'}}>
+      {favoriteListMock.map(favoriteListMock =>  <FavoriteItem favoriteListItem={favoriteListMock}/>)}
      
+  </div>
     </>
   );
 }
