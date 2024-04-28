968ef8f26d3067474816103ed592859d81db658c
kjwjwjw <tbs03112@naver.com>
2024-04-28 19:18:02 +0900

    footer

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
+import Footer from 'layouts/Footer';
 import './App.css';
-import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock } from 'mocks';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
+
 
 function App() {
 
-  const [ value, setValue] = useState<String>('');
+
 
   return (
     <>
-      <InputBox label='이메일' type='text' placeholder='이메일주소를 입력해주세요' value='value'  error={false}  setValue={setValue}/>
+      <Footer />
     </>
   );
 }
--- board-front/src/layouts/Footer/index.tsx
+++ board-front/src/layouts/Footer/index.tsx
@@ -9,6 +9,31 @@ export default function Footer() {
 
 //          render: 푸터 레이아웃 렌더링    
   return (
-    <div>index</div>
+    <div id='footer'>
+            <div className='footer-container'>
+                <div className='footer-top'>
+                    <div className='footer-logo-box'></div>
+                        <div className='icon-box'>
+                            <div className='icon logo-light-icon'></div>
+                        </div>
+                        <div className='footer-logo-text'></div>
+                    <div className='footer-link-box'>
+                        <div className='footer-email-link'></div>
+                        <div className='icon-button'>
+                            <div className='icon insta-icon'></div>
+                        </div>
+                        <div className='icon-button'>
+                            <div className='icon naver-icon'></div>
+                        </div>
+                    </div>
+                </div>
+                <div className='footer-botoom'>
+                    <div className='footer-copyright'></div>
+               </div>
+            </div>
+
+
+
+    </div>
   )
 }
