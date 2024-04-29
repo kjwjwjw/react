1dfe1628caf9dfebd7a7f9199308e6c9208e9ae9
kjwjwjw <tbs03112@naver.com>
2024-04-29 21:50:52 +0900

    React-Router-Dom SPA 작업

--- board-front/src/layouts/Footer/index.tsx
+++ board-front/src/layouts/Footer/index.tsx
@@ -7,6 +7,17 @@ import './style.css';
 export default function Footer() {
 
 
+    const onInstaIconButtonClickHandler = () => {
+            window.open('http://www.instagram.com');
+
+    }
+
+    
+    const onNaverBlogIconButtonClickHandler = () => {
+            window.open('https://blog.naver.com');
+
+    }
+
 //          render: 푸터 레이아웃 렌더링    
   return (
     <div id='footer'>
@@ -16,18 +27,18 @@ export default function Footer() {
                         <div className='icon-box'>
                             <div className='icon logo-light-icon'></div>
                         </div>
-                        <div className='footer-logo-text'></div>
+                        <div className='footer-logo-text'>{'Hoons Board'}</div>
                     <div className='footer-link-box'>
-                        <div className='footer-email-link'></div>
-                        <div className='icon-button'>
-                            <div className='icon insta-icon'></div>
+                        <div className='footer-email-link'>{'sdkfj@gmail.com'}</div>
+                        <div className='icon-button' onClick={onInstaIconButtonClickHandler}>
+                            <div className='icon insta-icon' ></div>
                         </div>
-                        <div className='icon-button'>
+                        <div className='icon-button' onClick={onNaverBlogIconButtonClickHandler}>
                             <div className='icon naver-icon'></div>
                         </div>
                     </div>
                 </div>
-                <div className='footer-botoom'>
+                <div className='footer-bottom'>
                     <div className='footer-copyright'></div>
                </div>
             </div>
---
+++ board-front/src/layouts/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css';
+
+//              component: Header       //
+
+export default function Header() {
+
+    // render : 헤더 렌더링             //
+    return (
+    <div>index</div>
+  )
+}
